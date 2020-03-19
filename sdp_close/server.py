2ca4cd5949a93d207df1b76e3d776d3b62b9a2f5
Alex format37 <format37@gmail.com>
2020-03-19 09:31:17 +0300

    new file:   sdp_close/ADD_WORKLOG.xml 	new file:   sdp_close/EDIT_REQUEST.xml 	new file:   sdp_close/server.py 	modified:   sdp_close/server.py

---
+++ sdp_close/server.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+import asyncio
+from aiohttp import web
+from urllib.parse import urlparse, parse_qsl
+import multidict as MultiDict
+import requests
+
+#@asyncio.coroutine
+async def bid_edit(request):
+	try:
+		SUBJECT		= request.rel_url.query['SUBJECT']
+	except:
+		SUBJECT		=''
+	try:
+		RESOLUTION	= request.rel_url.query['RESOLUTION']
+	except:
+		RESOLUTION	=''
+	try:
+		technician	= request.rel_url.query['TECHNICIAN']
+	except:
+		technician	=''
+	try:
+		workMinutes = request.rel_url.query['workMinutes']
+	except:
+		workMinutes =''
+	try:
+		workHours	= request.rel_url.query['workHours']
+	except:
+		workHours	=''
+	
+	content='''<html><body>
+		<form method="get" action="http://10.2.4.87:8080/bidclose">
+		<table>
+		  <tr>
+			<td>ID Заявки:</td>
+			<td><input name="WORKORDERID"> *</td>
+		  </tr>
+		  <tr>
+			<td>Тема:</td>
+			<td><input name="SUBJECT" value="'''+SUBJECT+'''"> *</td>			
+		  </tr>
+		  <tr>
+			<td>Заказчик:</td>
+			<td><input name="REQUESTER"></td>
+		  </tr>
+		  <tr>
+			<td>Исполнитель:</td>
+			<td><input name="TECHNICIAN" value="'''+technician+'''"> *</td>
+		  </tr>		  
+		  <tr>
+			<td>Заключение:</td>
+			<td><input name="RESOLUTION" value="'''+RESOLUTION+'''"> *</td>
+		  </tr>
+		  <tr>
+			<td>Минут:</td>
+			<td><input name="WORKMINUTES" value="'''+workMinutes+'''"></td>
+		  </tr>
+		  <tr>
+			<td>Часов:</td>
+			<td><input name="WORKHOURS" value="'''+workHours+'''"></td>
+		  </tr>
+		</table>
+		<br>		
+		<input type="submit" value="Закрыть">
+		</form>
+		</body></html>'''
+	return web.Response(text=content,content_type="text/html")
+
+async def bid_close(request):
+	
+	back_link='<br><input type="submit" value="Продолжить"><br><br>'
+	
+	try:
+		WORKORDERID = request.rel_url.query['WORKORDERID']
+	except:
+		print('Не заполнено поле ID Заявки'+back_link)
+		return
+		
+	try:
+		SUBJECT		= request.rel_url.query['SUBJECT']
+	except:
+		print('Не заполнено поле Тема'+back_link)
+		return
+	
+	try:
+		RESOLUTION	= request.rel_url.query['RESOLUTION']
+	except:
+		print('Не заполнено поле Заключение'+back_link)
+		return
+	
+	try:
+		technician	= request.rel_url.query['TECHNICIAN']
+	except:
+		print('Не заполнено поле Исполнитель'+back_link)
+		return
+	
+	try:
+		workMinutes = request.rel_url.query['WORKMINUTES']	
+	except:
+		workMinutes = ''
+		
+	try:
+		workHours	= request.rel_url.query['WORKHOURS']
+	except:
+		workHours	=''
+
+	try:
+		requester	= request.rel_url.query['REQUESTER']
+	except:
+		requester	= ''
+	
+	if workMinutes=='':
+		workMinutes='0'
+	if workHours=='':
+		workHours='0'
+
+	token='76ED27EB-D26D-412A-8151-5A65A16198E7'
+	edit_request_file='EDIT_REQUEST.xml'
+	add_worklog_file='ADD_WORKLOG.xml'
+	
+	#responce = 'WORKORDERID'+'<br>'+SUBJECT+'<br>'+RESOLUTION+'<br>'+technician+'<br>'+workMinutes+'<br>'+workHours+'<br>'+requester'
+	content='''<html><body>
+	<form method="get" action="http://10.2.4.87:8080/bidedit">
+	<input type="hidden" id="TECHNICIAN" name="TECHNICIAN" value="'''+technician+'''">
+	<input type="hidden" id="SUBJECT" name="SUBJECT" value="'''+SUBJECT+'''">
+	<input type="hidden" id="RESOLUTION" name="RESOLUTION" value="'''+RESOLUTION+'''">
+	<input type="hidden" id="workMinutes" name="workMinutes" value="'''+workMinutes+'''">
+	<input type="hidden" id="workHours" name="workHours" value="'''+workHours+'''">
+		<table>
+		  <tr>
+			<td>ID Заявки:</td>
+			<td>'''+WORKORDERID+'''</td>			
+		  </tr>
+		  <tr>
+			<td>Тема:</td>
+			<td>'''+SUBJECT+'''</td>			
+		  </tr>
+		  <tr>
+			<td>Заказчик:</td>
+			<td>'''+requester+'''</td>
+		  </tr>
+		  <tr>
+			<td>Исполнитель:</td>
+			<td>'''+technician+'''</td>
+		  </tr>		  
+		  <tr>
+			<td>Заключение:</td>
+			<td>'''+RESOLUTION+'''</td>
+		  </tr>
+		  <tr>
+			<td>Минут:</td>
+			<td>'''+workMinutes+'''</td>
+		  </tr>
+		  <tr>
+			<td>Часов:</td>
+			<td>'''+workHours+'''</td>
+		  </tr>
+		</table>
+		<br>'''	
+	
+	with open(add_worklog_file,'rb') as fh:
+		INPUT_DATA	= fh.read().decode("utf-8")
+		INPUT_DATA = INPUT_DATA.replace("%technician%", technician)
+		INPUT_DATA = INPUT_DATA.replace("%workMinutes%", workMinutes)
+		INPUT_DATA = INPUT_DATA.replace("%workHours%", workHours)
+		url='http://10.2.4.46/sdpapi/request/'+WORKORDERID+'/worklogs?OPERATION_NAME=ADD_WORKLOG&TECHNICIAN_KEY='+token+'&INPUT_DATA='+INPUT_DATA
+		headers = {'Content-Type': 'application/xml'}	
+		response = requests.post(url, headers=headers).text
+		content	+='<br>'+response.replace('\n','<br>')+'<br>'
+
+	with open(edit_request_file,'rb') as fh:
+		INPUT_DATA	= fh.read().decode("utf-8")
+		INPUT_DATA = INPUT_DATA.replace("%Subject%", SUBJECT)
+		INPUT_DATA = INPUT_DATA.replace("%Resolution%", RESOLUTION)
+		INPUT_DATA = INPUT_DATA.replace("%requester%", '' if requester=='' else '<parameter><name>requester</name><value>'+requester+'</value></parameter>')
+		url='http://10.2.4.46/sdpapi/request/'+WORKORDERID+'?OPERATION_NAME=EDIT_REQUEST&TECHNICIAN_KEY='+token+'&INPUT_DATA='+INPUT_DATA
+		headers = {'Content-Type': 'application/xml'}	
+		response = requests.post(url, headers=headers).text
+		content	+='<br>'+response.replace('\n','<br>')+'<br>'
+
+	content+=back_link
+	content+='<p><tt>'
+	content+='''
+	<br>-------------------YAao,---------------------------
+	<br>--------------------Y8888b,------------------------
+	<br>------------------,oA8888888b,---------------------
+	<br>------------,aaad8888888888888888bo,---------------
+	<br>---------,d888888888888888888888888888b,-----------
+	<br>-------,888888888888888888888888888888888b,--------
+	<br>------d8888888888888888888888888888888888888,------
+	<br>-----d888888888888888888888888888888888888888b-----
+	<br>----d888888P'--------------------`Y888888888888,---
+	<br>----88888P'--------------------Ybaaaa8888888888l---
+	<br>---a8888'----------------------`Y8888P'-`V888888---
+	<br>-d8888888a--------------------------------`Y8888---
+	<br>AY/''-`\Y8b---------------------------------``Y8b--
+	<br>Y'------`YP------------------------------------~~--'''
+	content+='</tt></p>'
+	content+='</form></body></html>'
+	return web.Response(text=content,content_type="text/html")
+
+app = web.Application()
+app.router.add_route('GET', '/bidedit', bid_edit)
+#app.router.add_route('GET', '/bidclose/{workorderid}/{subject}/{resolution}/{technician}/{workMinutes}/{workHours}/{requester}', bid_close)
+app.router.add_route('GET', '/bidclose', bid_close)
+
+loop = asyncio.get_event_loop()
+handler = app.make_handler()
+f = loop.create_server(handler, port='8080')
+srv = loop.run_until_complete(f)
+print('serving on', srv.sockets[0].getsockname())
+try:
+	loop.run_forever()
+except KeyboardInterrupt:
+	print("serving off...")
+finally:
+	loop.run_until_complete(handler.finish_connections(1.0))
+	srv.close()
