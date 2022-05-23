dfa752ecd2e0f5543def2dfdaf71dbdf416b4c22
Alexey Yurasov <13106906+format37@users.noreply.github.com>
2022-05-23 10:48:57 +0300

    debug: try removed

--- docker/jira_issue_creator/order_creator_daemon.py
+++ docker/jira_issue_creator/order_creator_daemon.py
@@ -57,143 +57,143 @@ def create_issue(jira, project,summary,description,accountId,issuetype,item):
 
 async def sdp_bid_create(created_by,caller_phone_number,department,receiver_phone_number):
 	
-	try:
-		print('\n======= sdp create by ats:',datetime.datetime.now())
-		sdp_order=''
-		technican=''
-		category=''
-		#created_by 				= request.rel_url.query['created_by']				# Петров М.В.
-		#caller_phone_number		= request.rel_url.query['caller_phone_number']		# 2001 - имя хоста от Nagios	
-		#department				= request.rel_url.query['department']				# MRM
-		#receiver_phone_number	= request.rel_url.query['receiver_phone_number']	# SIP/1611 - звонок принят
-		#api_key					= request.rel_url.query['api_key']					# API Key sdp
-		api_key = os.environ.get('API_KEY', '')
-		#sdp_key = os.environ.get('SDP_KEY', '')
+	#try:
+	print('\n======= sdp create by ats:',datetime.datetime.now())
+	sdp_order=''
+	technican=''
+	category=''
+	#created_by 				= request.rel_url.query['created_by']				# Петров М.В.
+	#caller_phone_number		= request.rel_url.query['caller_phone_number']		# 2001 - имя хоста от Nagios	
+	#department				= request.rel_url.query['department']				# MRM
+	#receiver_phone_number	= request.rel_url.query['receiver_phone_number']	# SIP/1611 - звонок принят
+	#api_key					= request.rel_url.query['api_key']					# API Key sdp
+	api_key = os.environ.get('API_KEY', '')
+	#sdp_key = os.environ.get('SDP_KEY', '')
 		
-		try:
-			http = urllib3.PoolManager()
-			url = 'http://10.2.4.52/service/servicedeskplus/-/raw/master/settings/technicans.txt'
-			response = http.request('GET', url)
-			technicans = eval(response.data.decode('utf-8'))
-		except Exception as e:
-			print('technicans.txt request error: '+str(e))
-			technicans={
-				'1611':'Сотников Артём Игоревич',
-				'1613':'Юрасов Алексей Александрович',
-				'1606':'Бывальцев Виктор Валентинович',
-				'1615':'Семенов Олег Владимирович',
-				'1601':'Кузьмин Евгений Андреевич',
-				'1602':'Дрожжин Николай Сергеевич',
-				'1501':'Васильев Дмитрий Александрович',
-				'1608':'Головин Олег Дмитриевич',
-				'1519':'Бойко Илья Вадимович',
-				'1607':'Титов Иван Сергеевич',
-				'2202':'Севрюкова Анна Юрьевна',
-				'1621':'Песоцкий Константин Вячеславович',
-			}
-		receiver_four_digit_phone=receiver_phone_number[-4:]
-		if receiver_four_digit_phone in technicans.keys():
-			technican = technicans[receiver_four_digit_phone]
-
-		subject		= created_by+' '+caller_phone_number+' '+department
-		description = subject+' Звонок принят '+receiver_phone_number+' '+technican
-		# create_request_file='/home/alex/projects/servicedeskplus/sdp_close/CREATE_REQUEST.xml'
-		create_request_file='CREATE_REQUEST.xml'
-
-		response = '0'
-		requester	= 'RoboTechnician'
-
-		try:
-			http = urllib3.PoolManager()
-			url = 'http://10.2.4.52/service/servicedeskplus/-/raw/master/settings/requesters.txt'
-			response = http.request('GET', url)
-			requesters = eval(response.data.decode('utf-8'))
-		except Exception as e:
-			print('requesters.txt request error: '+str(e))
-
-			requesters={
-				'1539':'Смолина Наталья Викторовна',
-				'1639':'Судакова Елена Викторовна',
-				'2001':'Муненко Максим Владимирович',
-				'1531':'Бухтин Андрей Борисович',
-				'1550':'Кузьмин Олег Юрьевич',
-				'1516':'Лапшина Инесса Николаевна',
-				'1610':'Хайтович Светлана Федоровна',
-				'1542':'Руссман Татьяна Викторовна',
-				'1509':'Кондратьев Сергей Александрович',
-				'1645':'Дробышева Анна Сергеевна',
-				'1802':'Буренкова Вера Леонидовна',
-				'1545':'Гундоров Илья Александрович',
-				'2003':'Петровский Виталий Игоревич',
-				'1536':'Смирнов Геннадий Валерьевич',
-				'1532':'Пономарев Павел Петрович',
-				'1557':'Глазкова Ольга Ивановна',
-				'1409':'Шабанова Ирина Викторовна',
-				'1569':'Голосина Мария Игоревна',
-				'1568':'Глушков Дмитрий Александрович',
-				'1534':'Лагутин Юрий Семенович',
-				'1579':'Тихонов Антон Витальевич',
-				'1520':'Палагин Андрей Юрьевич',
-				'1571':'Дворова Ольга Викторовна',
-				'1528':'Захаров Василий Павлович',
-				'1612':'Сычева Ольга Ивановна',
-				'1572':'Леонова Марина Анатольевна',
-				'1548':'Симанова Екатерина Юрьевна',
-				'1523':'Апряткин Александр Васильевич',
-				'1676':'Сионская Галина Андреевна',
-				'2004':'Самохин Олег Игоревич',
-				'1584':'Авдеева Валентина Валерьевна',
-				'1617':'Акинфиева Евгения Валерьевна',
-				'1527':'Кудинов Дмитрий Александрович',
-				'1545':'Гундоров Илья Александрович',
-				'2101':'Темгаев Павел Борисович',
-				'1618':'Захарова Наталья Юрьевна',
-			}
-
-		requesters.update(technicans)
-
-		if caller_phone_number in requesters.keys():
-			requester	= requesters[caller_phone_number]
-
-		print('created_by:',created_by)
-		print('requester',requester)
-		print('technican:',technican)
-		print('caller_phone_number:',caller_phone_number)
-		print('sdp_key:',api_key)
-		print('department:',department)
-		print('receiver_phone_number:',receiver_phone_number)
-		print('subject:',subject)
-	
-		with open(create_request_file,'rb') as fh:		
-			INPUT_DATA	= fh.read().decode("utf-8")
-			INPUT_DATA = INPUT_DATA.replace("%Subject%",	subject)
-			INPUT_DATA = INPUT_DATA.replace("%Technician%",	technican)
-			INPUT_DATA = INPUT_DATA.replace("%Category%",	category)
-			INPUT_DATA = INPUT_DATA.replace("%Description%",description)
-			INPUT_DATA = INPUT_DATA.replace("%Requester%",requester)
-			url='http://10.2.4.46/sdpapi/request/?OPERATION_NAME=ADD_REQUEST&TECHNICIAN_KEY='+api_key+'&INPUT_DATA='+urllib.parse.quote_plus(INPUT_DATA)
-			#print(INPUT_DATA)
-			headers = {'Content-Type': 'application/xml'}		
-			xmlData = requests.post(url, headers=headers).text
-			API = ET.fromstring(xmlData)
-			#print('xmlData',xmlData)
-			responce	= API[0]		
-			operation	= responce[0]
-			result		= operation[0]
-			status		= result[0]
-			print('status',status.text)
-			message		= result[1]
-			print('message',message.text)
-
-			details		= operation[1]
-			response = details[0].text;
-			print('sdp new order',details[0].text)
-			sdp_order=details[0].text
-			
+	try:
+		http = urllib3.PoolManager()
+		url = 'http://10.2.4.52/service/servicedeskplus/-/raw/master/settings/technicans.txt'
+		response = http.request('GET', url)
+		technicans = eval(response.data.decode('utf-8'))
 	except Exception as e:
-		message = str(datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S"))+\
-		'\njira order_creator_daemon error: '+str(e)
-		send_to_telegram(message)
+		print('technicans.txt request error: '+str(e))
+		technicans={
+			'1611':'Сотников Артём Игоревич',
+			'1613':'Юрасов Алексей Александрович',
+			'1606':'Бывальцев Виктор Валентинович',
+			'1615':'Семенов Олег Владимирович',
+			'1601':'Кузьмин Евгений Андреевич',
+			'1602':'Дрожжин Николай Сергеевич',
+			'1501':'Васильев Дмитрий Александрович',
+			'1608':'Головин Олег Дмитриевич',
+			'1519':'Бойко Илья Вадимович',
+			'1607':'Титов Иван Сергеевич',
+			'2202':'Севрюкова Анна Юрьевна',
+			'1621':'Песоцкий Константин Вячеславович',
+		}
+	receiver_four_digit_phone=receiver_phone_number[-4:]
+	if receiver_four_digit_phone in technicans.keys():
+		technican = technicans[receiver_four_digit_phone]
+
+	subject		= created_by+' '+caller_phone_number+' '+department
+	description = subject+' Звонок принят '+receiver_phone_number+' '+technican
+	# create_request_file='/home/alex/projects/servicedeskplus/sdp_close/CREATE_REQUEST.xml'
+	create_request_file='CREATE_REQUEST.xml'
+
+	response = '0'
+	requester	= 'RoboTechnician'
+
+	try:
+		http = urllib3.PoolManager()
+		url = 'http://10.2.4.52/service/servicedeskplus/-/raw/master/settings/requesters.txt'
+		response = http.request('GET', url)
+		requesters = eval(response.data.decode('utf-8'))
+	except Exception as e:
+		print('requesters.txt request error: '+str(e))
+
+		requesters={
+			'1539':'Смолина Наталья Викторовна',
+			'1639':'Судакова Елена Викторовна',
+			'2001':'Муненко Максим Владимирович',
+			'1531':'Бухтин Андрей Борисович',
+			'1550':'Кузьмин Олег Юрьевич',
+			'1516':'Лапшина Инесса Николаевна',
+			'1610':'Хайтович Светлана Федоровна',
+			'1542':'Руссман Татьяна Викторовна',
+			'1509':'Кондратьев Сергей Александрович',
+			'1645':'Дробышева Анна Сергеевна',
+			'1802':'Буренкова Вера Леонидовна',
+			'1545':'Гундоров Илья Александрович',
+			'2003':'Петровский Виталий Игоревич',
+			'1536':'Смирнов Геннадий Валерьевич',
+			'1532':'Пономарев Павел Петрович',
+			'1557':'Глазкова Ольга Ивановна',
+			'1409':'Шабанова Ирина Викторовна',
+			'1569':'Голосина Мария Игоревна',
+			'1568':'Глушков Дмитрий Александрович',
+			'1534':'Лагутин Юрий Семенович',
+			'1579':'Тихонов Антон Витальевич',
+			'1520':'Палагин Андрей Юрьевич',
+			'1571':'Дворова Ольга Викторовна',
+			'1528':'Захаров Василий Павлович',
+			'1612':'Сычева Ольга Ивановна',
+			'1572':'Леонова Марина Анатольевна',
+			'1548':'Симанова Екатерина Юрьевна',
+			'1523':'Апряткин Александр Васильевич',
+			'1676':'Сионская Галина Андреевна',
+			'2004':'Самохин Олег Игоревич',
+			'1584':'Авдеева Валентина Валерьевна',
+			'1617':'Акинфиева Евгения Валерьевна',
+			'1527':'Кудинов Дмитрий Александрович',
+			'1545':'Гундоров Илья Александрович',
+			'2101':'Темгаев Павел Борисович',
+			'1618':'Захарова Наталья Юрьевна',
+		}
+
+	requesters.update(technicans)
+
+	if caller_phone_number in requesters.keys():
+		requester	= requesters[caller_phone_number]
+
+	print('created_by:',created_by)
+	print('requester',requester)
+	print('technican:',technican)
+	print('caller_phone_number:',caller_phone_number)
+	print('sdp_key:',api_key)
+	print('department:',department)
+	print('receiver_phone_number:',receiver_phone_number)
+	print('subject:',subject)
+	
+	with open(create_request_file,'rb') as fh:		
+		INPUT_DATA	= fh.read().decode("utf-8")
+		INPUT_DATA = INPUT_DATA.replace("%Subject%",	subject)
+		INPUT_DATA = INPUT_DATA.replace("%Technician%",	technican)
+		INPUT_DATA = INPUT_DATA.replace("%Category%",	category)
+		INPUT_DATA = INPUT_DATA.replace("%Description%",description)
+		INPUT_DATA = INPUT_DATA.replace("%Requester%",requester)
+		url='http://10.2.4.46/sdpapi/request/?OPERATION_NAME=ADD_REQUEST&TECHNICIAN_KEY='+api_key+'&INPUT_DATA='+urllib.parse.quote_plus(INPUT_DATA)
+		#print(INPUT_DATA)
+		headers = {'Content-Type': 'application/xml'}		
+		xmlData = requests.post(url, headers=headers).text
+		API = ET.fromstring(xmlData)
+		#print('xmlData',xmlData)
+		responce	= API[0]		
+		operation	= responce[0]
+		result		= operation[0]
+		status		= result[0]
+		print('status',status.text)
+		message		= result[1]
+		print('message',message.text)
+
+		details		= operation[1]
+		response = details[0].text;
+		print('sdp new order',details[0].text)
+		sdp_order=details[0].text
+			
+	#except Exception as e:
+	#	message = str(datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S"))+\
+	#	'\njira order_creator_daemon error: '+str(e)
+	#	send_to_telegram(message)
 	
 	try:
 	
